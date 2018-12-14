cb02fd4a4e43c03263250f8950ce324bea212123
Olegjan <oleggolota17@gmail.com>
2018-12-14 14:19:38 +0200

    Add Unity project
    Add Unity project

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Vuforia;
+
+public class MoveCube : MonoBehaviour {
+
+    GameObject targetCube;
+    GameObject left;
+    GameObject right;
+    GameObject up;
+    GameObject down;
+    bool move;
+    void Start () {
+        left = GameObject.FindGameObjectWithTag("TriggerLeft");
+        right = GameObject.FindGameObjectWithTag("TriggerRight");
+        up = GameObject.FindGameObjectWithTag("TriggerUp");
+        down = GameObject.FindGameObjectWithTag("TriggerDown");
+        targetCube = left;
+        TrackableBehaviour.Status dd = new TrackableBehaviour.Status();
+    }
+	void FixedUpdate () {
+        if (move) { 
+        transform.position = Vector3.MoveTowards(transform.position, targetCube.transform.position, Time.deltaTime / 6);
+        }
+    }
+    private void OnTriggerEnter(Collider col) {
+        if (col.tag == "TriggerLeft") { targetCube = up; }
+        if (col.tag == "TriggerUp") { targetCube = right; }
+        if (col.tag == "TriggerRight") { targetCube = down; }
+        if (col.tag == "TriggerDown") { targetCube = left; }
+    }
+    private void OnBecameVisible() {
+        move = true;
+    }
+    private void OnBecameInvisible() {
+        move = false;
+    }
+
+    void OnTrackingFound()
+    {
+        Debug.Log("fhfhfhfhf");
+    }
+}
---
+++ Assets/Scripts/SortLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SortLogic : MonoBehaviour {
+    public GameObject[] parents;
+    public GameObject[] cubes;
+
+    public class myCubeSorter : IComparer
+    {
+        int IComparer.Compare(System.Object x, System.Object y)
+        {
+            return ((new CaseInsensitiveComparer()).Compare(((GameObject)x).name, ((GameObject)y).name));
+        }
+    }
+    void Start()
+    {
+        parents = GameObject.FindGameObjectsWithTag("Parents");
+        cubes = GameObject.FindGameObjectsWithTag("Player");
+    }
+    public void FindParents()
+    {
+        IComparer myComparer = new myCubeSorter();
+        Array.Sort(cubes, myComparer);
+        Array.Sort(parents, myComparer);
+
+        for (int i = 0; i < parents.Length; i++)
+
+            for (int j = 0; j < cubes.Length; j++)
+            {
+                if (parents[i].name == cubes[i].name)
+                    cubes[i].transform.SetParent(parents[i].transform);
+                cubes[i].transform.position = new Vector3(parents[i].transform.position.x, parents[i].transform.position.y, parents[i].transform.position.z);
+            }
+    }
+}
